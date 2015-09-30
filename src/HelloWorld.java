e1d677dde95a0ba74a08cf51046cfd8690cbb603
Khurram Ali <khurram.ali13d@gmail.com>
2015-09-30 10:44:40 -0700

    my little pony.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,17 +1,20 @@
 
 public class HelloWorld {
 	String message;
+	String pony;
 	
-	public HelloWorld(String message) {
+	public HelloWorld(String message, String pony) {
 		this.message = message;
+		this.pony = pony; 
 	}
 	
 	public void ToString() {
 		System.out.println(this.message);
+		System.out.println(this.pony);
 	}
 	
 	public static void main(String[] args) {
-		HelloWorld hw = new HelloWorld("Hello World!");
+		HelloWorld hw = new HelloWorld("Hello World!", "My Ltitle Pony!");
 		hw.ToString();
 	}
 	
