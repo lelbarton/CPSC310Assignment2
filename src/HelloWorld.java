ffff0cecfe281fafc7d637e45608756b09f2a584
Matthew Nyhus <o8b9@lulu.ugrad.cs.ubc.ca>
2015-09-29 18:58:18 -0700

    refactored code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,19 @@
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");		
+	String message;
+	
+	public HelloWorld(String message) {
+		this.message = message;
 	}
+	
+	public void ToString() {
+		System.out.println(this.message);
+	}
+	
+	public static void main(String[] args) {
+		HelloWorld hw = new HelloWorld("Hello World!");
+		hw.ToString();
+	}
+	
+	
 }
