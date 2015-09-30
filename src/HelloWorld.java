6a6fc067afe6c22f6e2a8b677dce41ce368b621d
Matthew Nyhus <o8b9@deas.ugrad.cs.ubc.ca>
2015-09-30 10:51:51 -0700

    Added number of repetitions

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,17 +1,21 @@
 
 public class HelloWorld {
 	String message;
-	
-	public HelloWorld(String message) {
+	int repeat;
+    
+	public HelloWorld(String message, int repeat) {
 		this.message = message;
+        this.repeat = repeat;
 	}
 	
 	public void ToString() {
-		System.out.println(this.message);
+		for (int i=0; i < repeat; i++) {
+            System.out.println(this.message);
+        }
 	}
 	
 	public static void main(String[] args) {
-		HelloWorld hw = new HelloWorld("Hello World!");
+		HelloWorld hw = new HelloWorld("Hello World!", 3);
 		hw.ToString();
 	}
 	
